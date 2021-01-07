31fe988ccf1f1af80974367a052be2d9e96c471d
domuk-k <dannyworks102@gmail.com>
2021-01-07 22:03:08 +0900

    feat: data-fetching and initial-render

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import App from './App';
-import { createVnode, render } from './lib';
+import { createVnode, Renderer } from './lib';
 import 'reset.css';
 import './index.scss';
 
-render(createVnode(App, null), document.getElementById('root'));
+const $root = document.getElementById('root');
+const AppNode = createVnode(App, null);
+
+export const renderer = new Renderer($root);
+
+renderer.render(AppNode);
